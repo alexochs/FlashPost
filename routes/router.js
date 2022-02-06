eb21d390ba9e22e13961b6623316eeeec535d901
yori21 <mail@alexochs.de>
2022-02-06 22:11:47 +0100

    Add nodemon extensions, ready to merge

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const authTwitterUrl = require("../index");
+
 router.get("/", (req, res) => {
-    res.render("index", { greeting: "Hello, World!" });
+    res.render("index", { authTwitter: "authTwitterUrl" });
 });
 
 module.exports = router;
