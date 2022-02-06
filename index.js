a825afb2e0c9e56a85f61c20d11d4feef3853a1d
yori21 <mail@alexochs.de>
2022-02-06 12:49:59 +0100

    Twitter v2 API and dotenv modules

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+const TwitterApi = require("twitter-api-v2").default;
+const dotenv = require("dotenv");
+dotenv.config();
+
+const twitter = new TwitterApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+});
+
+
+const authenticate = () => {
+    const { url, codeVerifier, state } = twitter.generateOAuth2AuthLink("https://alexochs.de", { scope: ['tweet.read', 'users.read', 'offline.access'] });
+    console.log(url);
+    console.log(codeVerifier);
+    console.log(state);
+};
+
+const tweet = () => {
+    const text = "Hello, World! via FlashPost(tm)";
+    results = twitter.v2.tweetCountRecent("elon musk");
+    console.log(results);
+};
+
 const main = () => {
-    console.log("Hello, World!");
+    authenticate();
 }
 
 main();
